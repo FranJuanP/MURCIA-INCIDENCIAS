e49fb44f803b7df16a3fe6454f5f4a3a1443749f
OkrtSystem Labs <juanf.perez@edu.uah.es>
2026-02-09 21:17:55 +0100

    ok

--- docs/config.js
+++
@@ -1,5 +0,0 @@
-window.API_BASE = "";
-window.DATA_INCIDENTS = "data/incidents.json";
-window.DATA_STATS     = "data/stats.json";
-window.DATA_SOURCES   = "data/sources.json";
-window.POLL_MS = 15000;
